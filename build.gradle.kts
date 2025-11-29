83c0d62e562b57e44956cbbfa9a8376fc101cfd3
Luna <lunaplugins@gmail.com>
2025-11-29 23:03:24 +0100

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("java")
+  id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+repositories {
+  maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+  compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+}
+
+tasks {
+  runServer {
+    minecraftVersion("1.21.10")
+    jvmArgs("-Xms4G", "-Xms4G", "-Dcom.mojang.eula.agree=true")
+  }
+
+  processResources {
+    val version: String = project.version.toString()
+    filesMatching("paper-plugin.yml") {
+      expand("version" to version)
+    }
+  }
+}
---
+++ src/main/java/dev/luna/economy/LunaEconomy.java
@@ -0,0 +1,13 @@
+package dev.luna.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+class LunaEconomy extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // ...
+  }
+}
